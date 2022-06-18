73ea53721e856b2a21d371a3c1d32871c03a9030
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-06-18 18:55:15 +0530

    feat:add rules to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,20 @@ const webpackConfig = {
     path: path.resolve(__dirname, "dist"),
   },
   mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
 
 module.exports = webpackConfig;
